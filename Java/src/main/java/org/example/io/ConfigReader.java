500b2a084672b05816fab964cda869f2d4fa869e
polymorph233 <91326644+polymorph233@users.noreply.github.com>
2024-04-05 23:10:01 +0200

    Added comments and documentations for clarifying my intent

--- Java/src/main/java/org/example/io/ConfigReader.java
+++ Java/src/main/java/org/example/io/ConfigReader.java
@@ -15,12 +15,25 @@ import java.util.stream.Collectors;
 
 public class ConfigReader {
 
+    /**
+     * File name to read from
+     */
     private String filename;
 
+    /**
+     * Initialize a new configuration reader
+     * @param filename the path of config file
+     */
     public ConfigReader(String filename) {
         this.filename = filename;
     }
 
+    /**
+     * This method suppress the IOException check of BufferedReader.readLine which prevent a fluent style
+     * of chaining Optional flatMap calls.
+     * @param reader the BufferReader to read
+     * @return a string of next line it reads, as the {@link BufferedReader#readLine()}
+     */
     private String readLineUnchecked(BufferedReader reader) {
         try {
             return reader.readLine();
@@ -30,46 +43,75 @@ public class ConfigReader {
     }
 
     public Game buildGame() {
+
+        // Open the file and handle potential IO Exceptions - that's caused by this file opening
+        // at the end of this try-block
         try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
 
+            // Read four first lines of the config file, to build four parameters:
+            // height, width, propagationRate, initCount
 
+            // Java lacks good support for null coalescing so we have to chain flatMaps
+            // but it prevents use from repeating 4 nested if-null checks (and handling IO exceptions)
             var configuration = Optional.ofNullable(readLineUnchecked(reader))
-                .flatMap(lengthLine -> Optional.ofNullable(readLineUnchecked(reader))
+                .flatMap(heightLine -> Optional.ofNullable(readLineUnchecked(reader))
                     .flatMap(widthLine -> Optional.ofNullable(readLineUnchecked(reader))
                         .flatMap(propLine -> Optional.ofNullable(readLineUnchecked(reader))
                             .flatMap(countLine ->{
-                                var length = Integer.parseInt(lengthLine.substring(8));
-                                var width = Integer.parseInt(widthLine.substring(7));
-                                var propagationRate = Double.parseDouble(propLine.substring(13));
-                                var initCount = Integer.parseInt(countLine.substring(12));
-                                var ret = new Configuration();
-                                ret.height = length;
-                                ret.width = width;
-                                ret.propagationRate = propagationRate;
-                                ret.initCount = initCount;
-                                return Optional.of(ret);
+                                // A rough try-block to parse numbers from 4 read lines and build a configuration object
+                                try {
+                                    var height = Integer.parseInt(heightLine.substring(8));
+                                    var width = Integer.parseInt(widthLine.substring(7));
+                                    var propagationRate = Double.parseDouble(propLine.substring(13));
+                                    var initCount = Integer.parseInt(countLine.substring(12));
+                                    var ret = new Configuration();
+                                    ret.height = height;
+                                    ret.width = width;
+                                    ret.propagationRate = propagationRate;
+                                    ret.initCount = initCount;
+                                    // If reached here, every thing is ok so a configuration object is built and
+                                    // it would be returned as an optional that holds it
+                                    return Optional.of(ret);
+                                } catch (Exception e) {
+                                    // Due to any reason, the parsing failed, so we return an optional which is empty
+                                    // (equiv. to null)
+                                    return Optional.empty();
+                                }
                             }))));
 
-            configuration.flatMap(conf -> {
+            // If configuration was successfully built, we know how many init. coo should be read, so proceed to next stage
+            configuration = configuration.flatMap(conf -> {
+                // Loop through `initCount` count, we profit from the for loop of Java
                 for (var i = 0; i < conf.initCount; i++) {
                     var line = readLineUnchecked(reader);
+                    // If no enough read, config will be set to empty optional
                     if (line == null) {
                         return Optional.empty();
                     } else {
-                        var currPair = Arrays.stream(line.split(", ")).map(Integer::parseInt).collect(Collectors.toList());
-                        conf.appendCoo(new Pair<>(currPair.get(0), currPair.get(1)));
+                        // Similar to previous try-block to catch any potential error
+                        try {
+                            var currPair = Arrays.stream(line.split(", ")).map(Integer::parseInt).collect(Collectors.toList());
+                            conf.appendCoo(new Pair<>(currPair.get(0), currPair.get(1)));
+                        } catch (Exception e) {
+                            // Any error raised will result in an empty optional
+                            return Optional.empty();
+                        }
                     }
                 }
                 return Optional.of(conf);
             });
 
+            // Check if optional holds the config object
             if (configuration.isPresent()) {
+                // It presents, so safe to get
                 var conf = configuration.get();
+                // Check if it's safe to build a game
                 if (conf.isValidConfiguration()) {
                     return new Game(conf.height, conf.width, conf.propagationRate, conf.coos);
                 }
             }
 
+            // Any other case that file is read but invalid
             throw new IllegalStateException("Invalid configuration during reader setup");
 
         } catch (IOException e) {
--- Java/src/main/java/org/example/io/Configuration.java
+++ Java/src/main/java/org/example/io/Configuration.java
@@ -5,7 +5,12 @@ import org.example.core.Pair;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A data type to hold configuration and check if it's valid
+ */
 public class Configuration {
+
+    // All fields are initially empty (null or empty list)
 
     Integer height = null;
     Integer width = null;
@@ -13,14 +18,24 @@ public class Configuration {
     Integer initCount = null;
     List<Pair<Integer, Integer>> coos = new ArrayList<>();
 
+    /**
+     * Check if current configuration object is valid
+     * @return true if it's valid or false
+     */
     boolean isValidConfiguration() {
         return height != null
                 && width != null
                 && propagationRate != null
                 && initCount != null
                 && coos.size() == initCount;
+
+        // Maybe we could check if height and width are strictly positive?
     }
 
+    /**
+     * Append a coordinate to this configuration
+     * @param coo a pair of ints (coordinate) to append
+     */
     void appendCoo(Pair<Integer, Integer> coo) {
         this.coos.add(coo);
     }
